8d5669424dccb1c236fe70794f41a4c4200c43f4
Ninja <cuansenputra@gmail.com>
2023-08-16 17:00:07 +0700

    set friend req

--- src/helper/redis.ts
+++ src/helper/redis.ts
@@ -1,2 +1,23 @@
-const upstashRedisRestUrl = process.env.UPSTASH_REDIS_REST_URL
-const upstashRedisRestToken = process.env.UPSTASH_REDIS_REST_TOKEN
+const upstashRedisRestUrl = process.env.UPSTASH_REDIS_REST_URL;
+const authToken = process.env.UPSTASH_REDIS_REST_TOKEN;
+
+type Command = 'zrange' | 'sismember' | 'get' | 'smembers';
+
+export async function fetchRedis(command: Command, ...args: (string | number)[]) {
+    const commanUrl = `${upstashRedisRestUrl}/${command}/${args.join('/')}`;
+
+    const response = await fetch(commanUrl, {
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+        },
+        cache: 'no-store'
+    });
+
+    if(!response.ok){
+        throw new Error(`Error executing Redis command ${response.statusText}`)
+    };
+
+    const data = await response.json();
+    return data.result;
+    
+};
